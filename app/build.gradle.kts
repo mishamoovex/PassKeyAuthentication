64973ac93752b45aa943de41c1cd12d4a21486e3
Mylhailo Vasylenko <mykhailo.development@gmail.com>
2024-01-18 23:36:22 +0200

    dependencies multimanagement

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,16 +1,19 @@
 plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.kotlinAndroid)
+    alias(libs.plugins.hilt)
+    kotlin("kapt")
+}
+
+apply {
+    from("$rootDir/feature.gradle")
 }
 
 android {
     namespace = "com.aksio.authentication"
-    compileSdk = libs.versions.gradle.compile.sdk.get().toInt()
 
     defaultConfig {
         applicationId = "com.aksio.authentication"
-        minSdk = libs.versions.gradle.min.sdk.get().toInt()
-        targetSdk = libs.versions.gradle.target.sdk.get().toInt()
         versionCode = 1
         versionName = "1.0"
 
@@ -28,19 +31,7 @@ android {
             )
         }
     }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
-    buildFeatures {
-        compose = true
-    }
-    composeOptions {
-        kotlinCompilerExtensionVersion = libs.versions.compose.compiller.get()
-    }
+
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
@@ -50,13 +41,21 @@ android {
 
 dependencies {
 
+    implementation(libs.splash.screen)
+    implementation(libs.lifecycle.vm)
+    //Hilt
+    implementation(libs.hilt)
+    implementation(libs.hilt.navigation)
+    kapt(libs.hilt.compiler)
+    //Compose
+    implementation(libs.compose.activity)
     implementation(platform(libs.compose.bom))
     androidTestImplementation(platform(libs.compose.bom))
-    implementation(libs.compose.tooling.preview)
     debugImplementation(libs.compose.tooling)
-    implementation(libs.compose.material3)
-    implementation(libs.compose.ui)
-    implementation(libs.compose.graphics)
-    implementation(libs.compose.activity)
+    implementation(libs.bundles.compose)
+}
 
+// Code generation for Hilt
+kapt {
+    correctErrorTypes = true
 }
