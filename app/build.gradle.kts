dde29abd4e4e0345bad5e50f104eebb114d6500e
Mykhailo Vasylemko <mykhailo.development@gmail.com>
2024-01-19 11:59:51 +0200

    dependencies setup (#1)
    * removed initial useless code
    
    * git ignore
    
    * set up basic dependencies
    
    * gradle properties
    
    * dependencies multimanagement
    
    * new feature module
    
    * common files
    
    * file update
    
    * updated gradle file management
    
    * removed usless config file
    
    * fixed crush
    
    * sample screen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,21 +1,19 @@
-@Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
 plugins {
     alias(libs.plugins.androidApplication)
-    alias(libs.plugins.kotlinAndroid)
+    kotlin("kapt")
 }
+
+apply(from = "$rootDir/gradle/common-feature.gradle")
 
 android {
     namespace = "com.aksio.authentication"
-    compileSdk = 34
 
     defaultConfig {
+        targetSdk = libs.versions.gradle.target.sdk.get().toInt()
         applicationId = "com.aksio.authentication"
-        minSdk = 26
-        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
             useSupportLibrary = true
         }
@@ -30,19 +28,7 @@ android {
             )
         }
     }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
-    buildFeatures {
-        compose = true
-    }
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.1"
-    }
+
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
@@ -51,20 +37,13 @@ android {
 }
 
 dependencies {
+    implementation(project(":features:authentication"))
 
-    implementation(libs.core.ktx)
-    implementation(libs.lifecycle.runtime.ktx)
-    implementation(libs.activity.compose)
-    implementation(platform(libs.compose.bom))
-    implementation(libs.ui)
-    implementation(libs.ui.graphics)
-    implementation(libs.ui.tooling.preview)
-    implementation(libs.material3)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.test.ext.junit)
-    androidTestImplementation(libs.espresso.core)
-    androidTestImplementation(platform(libs.compose.bom))
-    androidTestImplementation(libs.ui.test.junit4)
-    debugImplementation(libs.ui.tooling)
-    debugImplementation(libs.ui.test.manifest)
+    implementation(libs.splash.screen)
+    implementation(libs.compose.activity)
+}
+
+// Code generation for Hilt
+kapt {
+    correctErrorTypes = true
 }
--- app/src/main/java/com/aksio/authentication/MainActivity.kt
+++ app/src/main/java/com/aksio/authentication/MainActivity.kt
@@ -11,18 +11,19 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.aksio.authentication.ui.theme.AuthenticationTheme
+import com.aksio.features.authentication.WelcomeScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             AuthenticationTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
                     Greeting("Android")
+                    WelcomeScreen()
                 }
             }
         }
