03d0f067400a8a2800d5df65efc249ba196faa00
Mylhailo Vasylenko <mykhailo.development@gmail.com>
2024-01-19 16:43:56 +0200

    splashscreen

--- app/src/main/java/com/aksio/authentication/MainActivity.kt
+++ app/src/main/java/com/aksio/authentication/MainActivity.kt
@@ -10,12 +10,29 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.core.view.WindowCompat
+import androidx.lifecycle.lifecycleScope
 import com.aksio.core.designsystem.theme.AppTheme
 import com.aksio.features.authentication.WelcomeScreen
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import java.util.concurrent.atomic.AtomicBoolean
 
 class MainActivity : ComponentActivity() {
+
+    private val isSplashActive = AtomicBoolean(true)
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         super.onCreate(savedInstanceState)
+        installSplashScreen().setKeepOnScreenCondition { isSplashActive.get() }
+
+        lifecycleScope.launch {
+            delay(300)
+            isSplashActive.set(false)
+        }
+
         setContent {
             AppTheme {
                 Surface(
