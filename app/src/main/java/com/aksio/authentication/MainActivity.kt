4307cee21bd43a79889f878b717561437183ddb9
Mylhailo Vasylenko <mykhailo.development@gmail.com>
2024-01-19 16:37:17 +0200

    updated activity with the new theme

--- app/src/main/java/com/aksio/authentication/MainActivity.kt
+++ app/src/main/java/com/aksio/authentication/MainActivity.kt
@@ -10,14 +10,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.aksio.core.designsystem.theme.AuthenticationTheme
+import com.aksio.core.designsystem.theme.AppTheme
 import com.aksio.features.authentication.WelcomeScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            AuthenticationTheme {
+            AppTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
@@ -41,7 +41,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    AuthenticationTheme {
+    AppTheme {
         Greeting("Android")
     }
 }
