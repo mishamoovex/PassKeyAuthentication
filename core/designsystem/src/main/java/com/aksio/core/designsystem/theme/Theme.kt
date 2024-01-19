e4de727780aa4545e500c4d5301830a232f16369
Mylhailo Vasylenko <mykhailo.development@gmail.com>
2024-01-19 16:10:56 +0200

    theme setup

---
+++ core/designsystem/src/main/java/com/aksio/core/designsystem/theme/Theme.kt
@@ -0,0 +1,35 @@
+package com.aksio.core.designsystem.theme
+
+import androidx.compose.foundation.text.selection.LocalTextSelectionColors
+import androidx.compose.material.ripple.LocalRippleTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Typography
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+
+@Composable
+fun AuthenticationTheme(
+    content: @Composable () -> Unit
+) {
+    CompositionLocalProvider(LocalCustomColors provides LightColorPalette) {
+        MaterialTheme(
+            colorScheme = debugColors(),
+            typography = Typography
+        ) {
+            CompositionLocalProvider(
+                LocalTextSelectionColors provides TextSelectionsColors,
+                LocalRippleTheme provides AppRippleTheme,
+                content = content
+            )
+        }
+    }
+}
+
+object AppTheme {
+
+    val colors: CustomThemeColors
+        @Composable get() = LocalCustomColors.current
+
+    val typography: Typography
+        @Composable get() = MaterialTheme.typography
+}
