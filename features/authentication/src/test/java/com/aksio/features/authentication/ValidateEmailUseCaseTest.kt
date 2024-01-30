c7a2de24b4f2fbfc24bf51459dbdb049fd789a81
Mylhailo Vasylenko <mykhailo.development@gmail.com>
2024-01-30 12:01:02 +0200

    updated coroutine tests

--- features/authentication/src/test/java/com/aksio/features/authentication/ValidateEmailUseCaseTest.kt
+++ features/authentication/src/test/java/com/aksio/features/authentication/ValidateEmailUseCaseTest.kt
@@ -4,7 +4,7 @@ import com.aksio.core.common.state.TextMessage
 import com.aksio.features.authentication.domain.email.ValidateEmailUseCaseImpl
 import io.kotest.matchers.nulls.shouldBeNull
 import io.kotest.matchers.shouldBe
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.runTest
 import org.junit.Test
 
 class ValidateEmailUseCaseTest {
@@ -12,50 +12,38 @@ class ValidateEmailUseCaseTest {
     private val useCase = ValidateEmailUseCaseImpl()
 
     @Test
-    fun `SHOULD return null WHEN email is valid`() {
-        runBlocking {
-            val result = useCase.invoke("email@bank.com")
-            result.shouldBeNull()
-        }
+    fun `SHOULD return null WHEN email is valid`() = runTest {
+        val result = useCase.invoke("email@bank.com")
+        result.shouldBeNull()
     }
 
     @Test
-    fun `SHOULD return an empty error WHEN email is empty`() {
-        runBlocking {
-            val result = useCase.invoke("")
-            result shouldBe TextMessage.ResourceMessage(R.string.text_validation_error_empty_email)
-        }
+    fun `SHOULD return an empty error WHEN email is empty`() = runTest {
+        val result = useCase.invoke("")
+        result shouldBe TextMessage.ResourceMessage(R.string.text_validation_error_empty_email)
     }
 
     @Test
-    fun `SHOULD return an invalid error WHEN domain separator is missed`() {
-        runBlocking {
-            val result = useCase.invoke("email.com")
-            result shouldBe TextMessage.ResourceMessage(R.string.text_validation_error_invalid_email)
-        }
+    fun `SHOULD return an invalid error WHEN domain separator is missed`() = runTest {
+        val result = useCase.invoke("email.com")
+        result shouldBe TextMessage.ResourceMessage(R.string.text_validation_error_invalid_email)
     }
 
     @Test
-    fun `SHOULD return an invalid error WHEN sub domain separator is missed`() {
-        runBlocking {
-            val result = useCase.invoke("email@com")
-            result shouldBe TextMessage.ResourceMessage(R.string.text_validation_error_invalid_email)
-        }
+    fun `SHOULD return an invalid error WHEN sub domain separator is missed`() = runTest {
+        val result = useCase.invoke("email@com")
+        result shouldBe TextMessage.ResourceMessage(R.string.text_validation_error_invalid_email)
     }
 
     @Test
-    fun `SHOULD return an invalid error WHEN email don't have domain separators`() {
-        runBlocking {
-            val result = useCase.invoke("email")
-            result shouldBe TextMessage.ResourceMessage(R.string.text_validation_error_invalid_email)
-        }
+    fun `SHOULD return an invalid error WHEN email don't have domain separators`() = runTest {
+        val result = useCase.invoke("email")
+        result shouldBe TextMessage.ResourceMessage(R.string.text_validation_error_invalid_email)
     }
 
     @Test
-    fun `SHOULD return an invalid error WHEN email contains special characters`() {
-        runBlocking {
-            val result = useCase.invoke("##$%@gmail.com")
-            result shouldBe TextMessage.ResourceMessage(R.string.text_validation_error_invalid_email)
-        }
+    fun `SHOULD return an invalid error WHEN email contains special characters`() = runTest {
+        val result = useCase.invoke("##$%@gmail.com")
+        result shouldBe TextMessage.ResourceMessage(R.string.text_validation_error_invalid_email)
     }
 }
