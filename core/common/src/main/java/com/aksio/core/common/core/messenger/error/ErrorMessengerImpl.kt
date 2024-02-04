a4509c76ba99f2161238549aa866dcd8a0ef5e4d
Mylhailo Vasylenko <mykhailo.development@gmail.com>
2024-02-04 19:21:44 +0200

    ErrorMessenger tests

--- core/common/src/main/java/com/aksio/core/common/core/messenger/error/ErrorMessengerImpl.kt
+++ core/common/src/main/java/com/aksio/core/common/core/messenger/error/ErrorMessengerImpl.kt
@@ -2,9 +2,6 @@ package com.aksio.core.common.core.messenger.error
 
 import com.aksio.core.common.core.messenger.base.Messenger
 import com.aksio.core.common.state.TextMessage
-import java.net.ConnectException
-import java.net.UnknownHostException
-import java.util.concurrent.TimeoutException
 import javax.inject.Inject
 
 class ErrorMessengerImpl @Inject constructor(
@@ -21,12 +18,5 @@ class ErrorMessengerImpl @Inject constructor(
         TextMessage.StringMessage(throwable.rationale)
 
     private val Throwable.rationale: String
-        get() = if (isNoInternetException()) {
-            "No Internet Connection"
-        } else {
-            message ?: "Unknown error"
-        }
-
-    private fun Throwable.isNoInternetException() =
-        this is TimeoutException || this is UnknownHostException || this is ConnectException
+        get() = message ?: "Unknown error"
 }
