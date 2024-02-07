15116e221d3f957d5f872f80b752995f90ce0970
Mylhailo Vasylenko <mykhailo.development@gmail.com>
2024-02-07 15:32:59 +0200

    email state validation logic implementation

---
+++ core/tests/src/main/java/com/aksio/core/tests_shared/util/StateFlowCollector.kt
@@ -0,0 +1,21 @@
+package com.aksio.core.tests_shared.util
+
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.test.TestScope
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
+
+/**
+ *  Activates [StateFlow] by assigning a collector.
+ *
+ *  We are not able to test [StateFlow]'s that have been created by the stateIn() function
+ *  using flow.value before it gets at least one collector.
+ */
+@OptIn(ExperimentalCoroutinesApi::class)
+fun <T> TestScope.setFlowCollector(flow: StateFlow<T>) {
+    backgroundScope.launch(UnconfinedTestDispatcher(testScheduler)) {
+        flow.collect()
+    }
+}
--- features/authentication/src/test/java/com/aksio/features/authentication/ui/email/resigter/screen/EmailSignUpVmTest.kt
+++ features/authentication/src/test/java/com/aksio/features/authentication/ui/email/resigter/screen/EmailSignUpVmTest.kt
@@ -1,10 +1,15 @@
 package com.aksio.features.authentication.ui.email.resigter.screen
 
+import com.aksio.core.common.state.ValidationState
 import com.aksio.core.tests_shared.MainDispatcherRule
 import com.aksio.core.tests_shared.fake.common.FakeClock
 import com.aksio.core.tests_shared.fake.common.FakeErrorMessenger
 import com.aksio.core.tests_shared.fake.repository.FakeAuthenticationRepository
+import com.aksio.core.tests_shared.util.setFlowCollector
 import com.aksio.features.authentication.fake.FakeStringValidation
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.string.shouldBeEmpty
+import kotlinx.coroutines.test.runTest
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -38,39 +43,83 @@ internal class EmailSignUpVmTest {
     //////////// Email state tests ///////////////
 
     @Test
-    fun `SHOULD set empty string as email value WHEN email state created`() {
-
+    fun `SHOULD set empty string as email value WHEN email state created`() = runTest {
+        //Given a fresh ViewModel
+        //When verifying initial state
+        //Than the email state text value should be an empty string
+        viewModel.uiState.value.emailState.value.shouldBeEmpty()
     }
 
     @Test
-    fun `SHOULD update email value value WHEN new value set`() {
-
+    fun `SHOULD update email value WHEN new value of required length is set`() = runTest {
+        setFlowCollector(viewModel.uiState)
+        //Given a valid email
+        val newEmail = "some@gmail.com"
+        //When new email is set
+        viewModel.uiState.value.emailState.onValueChanged(newEmail)
+        //Than the email state value should then be updated to the new email
+        viewModel.uiState.value.emailState.value.shouldBe(newEmail)
     }
 
     @Test
-    fun `SHOULD set email validation state Pending WHEN email state created`() {
-
+    fun `SHOULD set email validation state Pending WHEN email state created`() = runTest {
+        //Given a fresh ViewModel
+        //When verifying initial state
+        //Than the validation state should be Pending
+        viewModel.uiState.value.emailState.validationState.shouldBe(ValidationState.Pending)
     }
 
     @Test
-    fun `SHOULD set email validation state Pending WHEN email length less than required`() {
-
-    }
-
-    @Test
-    fun `SHOULD set email validation state Invalid WHEN email length longer than required`() {
-
-    }
-
-    @Test
-    fun `SHOULD set email validation state Invalid WHEN email length equals required length and not valid`() {
-
-    }
+    fun `SHOULD set email validation state Pending WHEN email length less than required`() =
+        runTest {
+            setFlowCollector(viewModel.uiState)
+            //Given a string of length less then required
+            val value = "1234"
+            //When new value is set
+            viewModel.uiState.value.emailState.onValueChanged(value)
+            //Than the validation state should be Pending
+            viewModel.uiState.value.emailState.validationState.shouldBe(ValidationState.Pending)
+        }
 
     @Test
-    fun `SHOULD set email validation state Valid WHEN email is valid`() {
+    fun `SHOULD set email validation state Invalid WHEN email length longer than required`() =
+        runTest {
+            setFlowCollector(viewModel.uiState)
+            //Given a string of length longer then required
+            val value = "1234567"
+            //When new value is set
+            emailValidationUseCase.isValid(isValid = false)
+            viewModel.uiState.value.emailState.onValueChanged(value)
+            //Than the validation state should be Invalid
+            val expectedState = ValidationState.Invalid(emailValidationUseCase.errorMessage)
+            viewModel.uiState.value.emailState.validationState.shouldBe(expectedState)
+        }
 
+    @Test
+    fun `SHOULD set email validation state Invalid WHEN email length equals required length and not valid`() =
+        runTest {
+            setFlowCollector(viewModel.uiState)
+            //Given an string that represents an email of required length
+            val value = "123456"
+            //When new value is set
+            emailValidationUseCase.isValid(isValid = false)
+            viewModel.uiState.value.emailState.onValueChanged(value)
+            //Than the validation state should be Invalid
+            val expectedState = ValidationState.Invalid(emailValidationUseCase.errorMessage)
+            viewModel.uiState.value.emailState.validationState.shouldBe(expectedState)
+        }
+
+    @Test
+    fun `SHOULD set email validation state Valid WHEN email is valid`() = runTest {
+        setFlowCollector(viewModel.uiState)
+        //Given a valid email
+        val value = "some@gmail.com"
+        //When new email is set
+        viewModel.uiState.value.emailState.onValueChanged(value)
+        //Than the validation state should be Valid
+        viewModel.uiState.value.emailState.validationState.shouldBe(ValidationState.Valid)
     }
 
+    //////////// Password state tests ///////////////
 
 }
