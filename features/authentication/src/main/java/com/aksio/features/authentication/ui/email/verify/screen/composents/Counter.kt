56f4bd16381385f8cb8cead7970cd579df4f39a3
Mylhailo Vasylenko <mykhailo.development@gmail.com>
2024-02-09 13:51:57 +0200

    fixed counter

--- features/authentication/src/main/java/com/aksio/features/authentication/ui/email/verify/screen/composents/Counter.kt
+++ features/authentication/src/main/java/com/aksio/features/authentication/ui/email/verify/screen/composents/Counter.kt
@@ -5,14 +5,18 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -32,11 +36,12 @@ internal fun Counter(
     deadline: OffsetDateTime,
     actionButtonState: ActionButtonState
 ) {
-    var remaining = remember(deadline) {
+    var remaining by remember(deadline) {
         val nowDuration = OffsetDateTime.now().toEpochSecond().seconds
         val deadlineDuration = deadline.toEpochSecond().seconds
-        deadlineDuration - nowDuration
+        mutableStateOf(deadlineDuration - nowDuration)
     }
+
     val showActionBtn by remember {
         derivedStateOf { remaining.isNegative() || remaining == Duration.ZERO }
     }
@@ -44,7 +49,12 @@ internal fun Counter(
     Box(
         modifier = modifier
             .fillMaxWidth()
-            .height(56.dp),
+            .height(56.dp)
+            .clip(RoundedCornerShape(percent = 50))
+            .clickable(
+                enabled = showActionBtn,
+                onClick = actionButtonState.onClicked
+            ),
         contentAlignment = Alignment.Center
     ) {
 
@@ -58,8 +68,7 @@ internal fun Counter(
                     text = stringResource(R.string.email_verification_resend_email_btn),
                     style = AppTheme.typography.button.copy(
                         color = AppTheme.colors.onSecondaryButton
-                    ),
-                    modifier = Modifier.clickable(onClick = actionButtonState.onClicked)
+                    )
                 )
             }
         } else {
